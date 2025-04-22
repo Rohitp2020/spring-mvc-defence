14ca644f7d56d0b0ab4cc4a197a0d1f31d601ab2
rohit.punetha <rohit.punetha@LPT5288.newgen.co.in>
2025-04-22 11:13:34 +0530

    db

--- src/main/java/com/springsecurity/defence/UserEntity.java
+++ src/main/java/com/springsecurity/defence/UserEntity.java
@@ -14,7 +14,6 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Column(unique = true)
     private String email;
     private String password;
     private String role;
